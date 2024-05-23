349abac14109a9cfda88b21920e15f1d7a01ec42
AubreySwen <swensona@uwplatt.edu>
2024-05-23 10:52:45 -0500

    Attempting to use rigidbody

--- TheGame/Assets/Movement.cs
+++ TheGame/Assets/Movement.cs
@@ -1,33 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 50.0f;
+    public Rigidbody2D rigidbody2D;
+    public Vector2 movement;
+    public Vector2 temp;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform tempTransform = gameObject.GetComponent<Transform>();
-        Vector3 tempPosition = new Vector3(tempTransform.position.x, tempTransform.position.y, tempTransform.position.z);
+        //Transform tempTransform = gameObject.GetComponent<Transform>();
+        //Vector3 tempPosition = new Vector3(tempTransform.position.x, tempTransform.position.y, tempTransform.position.z);
 
-        if (Input.GetKeyDown(KeyCode.A))
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    tempPosition.x = tempPosition.x - speed;
+        //    tempTransform.position = tempPosition;
+        //}
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    tempPosition.x = tempPosition.x + speed;
+        //    tempTransform.position = tempPosition;
+        //}
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
         {
-            tempPosition.x = tempPosition.x - speed;
-            tempTransform.position = tempPosition;
+            movement = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
+            movement.y = rigidbody2D.velocity.y + (Input.GetAxis("Vertical") * speed * Time.deltaTime); // fix; constant increase of speed upwards
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        else
         {
-            tempPosition.x = tempPosition.x + speed;
-            tempTransform.position = tempPosition;
+            movement = new Vector2(0, rigidbody2D.velocity.y);
         }
-        
+        rigidbody2D.velocity = movement;
+
+        // stop horizontal movement and go up until let go then down
+
+        // double jump
+        // raycast downward
+
     }
+
+
 }
